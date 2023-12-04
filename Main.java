56736aeb568a38b0ca5fd9e6a8cd6cba7fdf1001
JulieHum <144722362+JulieHum@users.noreply.github.com>
2023-12-04 01:33:04 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        Algorithm.numOfDigits(0);
-        Algorithm.numOfDigits(-10);
-        Algorithm.divisible(10, 5);
-        Algorithm.divisible(1, 16);
-        Algorithm.sumOfOdds(0, 1);
-        Algorithm.sumOfOdds(-9, -4);
-        Algorithm.sumOfMultiplesTen(-21, -11);
-        Algorithm.sumOfMultiplesTen(0, 10);
-        Algorithm.sumOfParameterDigits(-199);
-        Algorithm.sumOfParameterDigits(-19);
-        Algorithm.largestPowerTwo(511);
-        Algorithm.largestPowerTwo(5);
+        System.out.println(Algorithm.secondInFirst("Hello World", "World"));
+        System.out.println(Algorithm.secondInFirst("I love oranges.", "i love"));
+        System.out.println(Algorithm.secondInFirst("AP CSA", "AP"));
+        System.out.println(Algorithm.stringReversed("gohangasalami"));
+        System.out.println(Algorithm.stringReversed("i ate all the fruit"));
+        System.out.println(Algorithm.stringReversed("Dogs are so cute."));
+        System.out.println(Algorithm.palindrome("amanaplanacanalpanama"));
+        System.out.println(Algorithm.palindrome("racecar"));
+        System.out.println(Algorithm.palindrome("garbage"));
+        System.out.println(Algorithm.occurrences("Mississippi", "si"));
+        System.out.println(Algorithm.occurrences("dogs cats gods dogs cat", "cat"));
+        System.out.println(Algorithm.occurrences("8 8 8 8 3 8 3", "8"));
+
+
+
+
+
+
+// 4.1 while Loops
+//        System.out.println(Algorithm.numOfDigits(2000));
+//        System.out.println(Algorithm.numOfDigits(-10));
+//        System.out.println(Algorithm.divisible(10, 5));
+//        System.out.println(Algorithm.divisible(1, 16));
+//        System.out.println(Algorithm.sumOfOdds(0, 1));
+//        System.out.println(Algorithm.sumOfOdds(-9, -4));
+//        System.out.println(Algorithm.sumOfMultiplesTen(-21, -11));
+//        System.out.println(Algorithm.sumOfMultiplesTen(0, 10));
+//        System.out.println(Algorithm.sumOfParameterDigits(-199));
+//        System.out.println(Algorithm.sumOfParameterDigits(-19));
+//        System.out.println(Algorithm.largestPowerTwo(511));
+//        System.out.println(Algorithm.largestPowerTwo(5));
     }
 }
