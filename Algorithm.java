0bae7fc34b7ca0e999d594454103f36c238fcbf7
BT_1N3_01 <julieh11@nycstudents.net>
2024-02-01 14:05:44 -0500

    d

--- Algorithm.java
+++ Algorithm.java
@@ -1,108 +1,30 @@
 public class Algorithm {
-    // 4.1 while Loops
-    public static int numOfDigits(int i) {
-        int digits = 1;
-        while (Math.abs(i) >= 10) {
-            i /= 10;
-            digits++;
-        }
-        return digits;
-    }
-
-
-    public static boolean divisible(int f, int s) {
-        return (f % s) == 0;
-
-    }
-
-    public static int sumOfOdds(int i, int j) {
-        int sum = 0;
-        while (j >= i) {
-            if ((Math.abs(i) % 2) == 1) {
-                sum += i;
-                i += 2;
-            } else {
-                i += 1;
-            }
-        }
-        return sum;
-    }
-
-    public static int sumOfMultiplesTen(int i, int j) {
-        int sum = 0;
-        while (j >= i) {
-            if ((i % 10) == 0) {
-                sum += i;
-                i += 10;
-            } else {
-                if (i >= 0) {
-                    i += (10 - (i % 10));
-                } else {
-                    i += (Math.abs(i) % 10);
-                }
-            }
-        }
-        return sum;
-    }
-
-    public static int sumOfParameterDigits(int i) {
-        int sum = 0;
-        i = Math.abs(i);
-        while (i != 0) {
-            sum += (i % 10);
-            i /= 10;
-        }
-        return sum;
-    }
-
-    public static int largestPowerTwo(int i) {
-        int p = 0;
-        while (Math.pow(2, p) <= i) {
-            p++;
-        }
-        return (int) Math.pow(2, p - 1);
-    }
-
-
-
-
-    // 4.3 Developing Algorithms Using Strings
-    public static boolean secondInFirst(String s, String t) {
-        boolean tf = false;
-        for (int i = 0; i <= (s.length() - t.length()); i++) {
-            if (s.substring(i, i + t.length()).equals(t)) {
-                tf = true;
-            }
-        }
-        return tf;
-    }
-
-    public static String stringReversed(String s) {
-        String newString = "";
-        for (int i = s.length(); i > 0; i--)
-            newString += s.substring(i - 1, i);
-        return newString;
-    }
-
-    public static boolean palindrome(String s) { // does not exclude special symbols
-        return stringReversed(s).compareTo(s) == 0;
-    }
-
-    public static int occurrences (String s, String t)
+    public static void print (int[] input)
     {
-        String n = s;
-        int place;
-        int occurrence = 0;
-        for (int i = 0; i < n.length(); i++)
+        for(int i = 0; i < input.length; i++)
         {
-            place = n.indexOf(t);
-            if (place != -1)
-            {
-                n = n.substring(place + t.length());
-                occurrence++;
-            }
+            System.out.print(input[i] + " ");
         }
-        return occurrence;
+
+    }
+//    public static int swap (int[] input, int s1, int s2)
+//    {
+//        int n = input[s2];
+//        input[s2] = input[s1];
+//        input[s1] = n;
+//        for(int i = 0; i < input.length; i++)
+//        {
+//            return input[i];
+//        }
+//
+//    }
+    public static int max (int[] input)
+    {
+        int n = 0;
+        for(int i = 0; i < input.length; i++)
+        {
+
+        }
     }
 }
 
--- Main.java
+++ Main.java
@@ -1,35 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Algorithm.secondInFirst("Hello World", "World"));
-        System.out.println(Algorithm.secondInFirst("I love oranges.", "i love"));
-        System.out.println(Algorithm.secondInFirst("AP CSA", "AP"));
-        System.out.println(Algorithm.stringReversed("gohangasalami"));
-        System.out.println(Algorithm.stringReversed("i ate all the fruit"));
-        System.out.println(Algorithm.stringReversed("Dogs are so cute."));
-        System.out.println(Algorithm.palindrome("amanaplanacanalpanama"));
-        System.out.println(Algorithm.palindrome("racecar"));
-        System.out.println(Algorithm.palindrome("garbage"));
-        System.out.println(Algorithm.occurrences("Mississippi", "si"));
-        System.out.println(Algorithm.occurrences("dogs cats gods dogs cat", "cat"));
-        System.out.println(Algorithm.occurrences("8 8 8 8 3 8 3", "8"));
+        int [] ints1 = {1, 2, 3};
+        int [] ints2 = {1, 2, 3, 4};
+        int [] ints3 = {3, 2, 1};
+        Algorithm.print(ints1);
+        Algorithm.print(ints2);
+        Algorithm.print(ints3);
 
-
-
-
-
-
-// 4.1 while Loops
-//        System.out.println(Algorithm.numOfDigits(2000));
-//        System.out.println(Algorithm.numOfDigits(-10));
-//        System.out.println(Algorithm.divisible(10, 5));
-//        System.out.println(Algorithm.divisible(1, 16));
-//        System.out.println(Algorithm.sumOfOdds(0, 1));
-//        System.out.println(Algorithm.sumOfOdds(-9, -4));
-//        System.out.println(Algorithm.sumOfMultiplesTen(-21, -11));
-//        System.out.println(Algorithm.sumOfMultiplesTen(0, 10));
-//        System.out.println(Algorithm.sumOfParameterDigits(-199));
-//        System.out.println(Algorithm.sumOfParameterDigits(-19));
-//        System.out.println(Algorithm.largestPowerTwo(511));
-//        System.out.println(Algorithm.largestPowerTwo(5));
+//        System.out.println(Algorithm.swap (ints2, 1, 2));
     }
 }
