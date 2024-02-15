4a1561eafefae963a89fae0161dd5957c24bf612
JulieHum <144722362+JulieHum@users.noreply.github.com>
2024-02-15 14:03:14 -0500

    Update Main.java
    class

--- Main.java
+++ Main.java
@@ -4,5 +4,36 @@ public class Main
     public static void main (String [] args)
     {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        Algorithm.reverseQ(ints1, ints2);
+        Algorithm.reverseQ(ints2, ints3);
+        Algorithm.reverseQ(ints3, ints4);
+        Algorithm.maximum
     }
 }
