24698869b716ae1e8574b8d70a626b587ce63e1e
Julie Hum <julieehum@gmail.com>
2024-02-19 15:13:27 -0500

    7.5 Searching

--- Algorithm.java
+++ Algorithm.java
@@ -1,165 +1,26 @@
-//7.4 Developing Algorithms Using ArrayLists
+//7.5 Searching
 import java.util.ArrayList;
+
 public class Algorithm
 {
-    public static void removeConsec (ArrayList<Integer> input)
+    public static int nLessThan0 (ArrayList<Integer> input)
     {
-        for (int i = 1; i < input.size(); i++)
+        int c = 0;
+        for (int i : input)
         {
-            if (input.get(i).equals(input.get(i-1)))
+            if (i < 0)
             {
-                input.remove(i);
-                i--;
+                c++;
             }
         }
+        return c;
     }
 
-    public static void remove (ArrayList<Integer> input, int i)
+    public static boolean inQ (ArrayList<String> input, String str)
     {
-        for (int j = 0; j < input.size(); j++)
-        {
-            if (input.get(j) == i)
-            {
-                input.remove(j);
-                j--;
-            }
-        }
-    }
-
-    public static void addInt (ArrayList<Integer> input, int i)
-    {
-        int added = 0;
-        if (input.size() == 0)
-        {
-            input.add(i);
-        }
-        for (int j = 0; j < input.size()-1; j++)
-        {
-            if (input.get(j) <= i && input.get(j+1) >= i && added == 0)
-            {
-                j++;
-                input.add(j, i);
-                added++;
-            }
-        }
-        if (i > input.get(input.size()-1))
-        {
-            input.add(input.size(), i);
-        }
-
-    }
-
-    public static boolean reverseQ (ArrayList<Integer> input, ArrayList<Integer> input1)
-    {
-        if (input.size() != input1.size())
-        {
-            return false;
-        }
-        for (int i = 0; i < input.size(); i++)
-        {
-            if (!(input.get(i)).equals(input1.get(input1.size()-1-i)))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static int maximum (ArrayList<Integer> input)
-    {
-        int i = input.get(0);
-        for (int j : input)
-        {
-            if (j > i)
-            {
-                i = j;
-            }
-        }
-        return i;
-    }
-
-    public static int minimum (ArrayList<Integer> input)
-    {
-        int i = input.get(0);
-        for (int j : input)
-        {
-            if (j < i)
-            {
-                i = j;
-            }
-        }
-        return i;
-    }
-
-    public static String shortest (ArrayList<String> input)
-    {
-        String str = input.get(0);
         for (String s : input)
         {
-            if (s.length() < str.length())
-            {
-                str = s;
-            }
-        }
-        return str;
-    }
-
-    public static String longest (ArrayList<String> input)
-    {
-        String str = input.get(0);
-        for (String s : input)
-        {
-            if (s.length() > str.length())
-            {
-                str = s;
-            }
-        }
-        return str;
-    }
-
-    public static int largestAbsDiff (ArrayList<Integer> input, ArrayList<Integer> input1)
-    {
-        int i = 0;
-        if (input.size() != input1.size())
-        {
-            return -1;
-        }
-        for (int j = 0; j < input.size(); j++)
-        {
-            if (Math.abs(input.get(j) - input1.get(j)) > i)
-            {
-                i = Math.abs(input.get(j) - input1.get(j));
-            }
-        }
-        return i;
-    }
-
-    public static double average (ArrayList<Double> input)
-    {
-        double sum = 0.0;
-        for (double d : input)
-        {
-            sum += d;
-        }
-        return sum/input.size();
-    }
-
-    public static void reverse (ArrayList<Integer> input)
-    {
-        int n = 0;
-        for (int i = 0; i < input.size()/2 ; i++)
-        {
-            n = input.get(i);
-            input.set(i, input.get(input.size()-1-i));
-            input.set(input.size()-1-i, n);
-        }
-    }
-
-    public static boolean consecQ (ArrayList<Integer> input)
-    {
-        for (int i = 1; i < input.size(); i++)
-        {
-            if (input.get(i).equals(input.get(i-1)))
+            if (s.equals(str))
             {
                 return true;
             }
@@ -167,30 +28,42 @@ public class Algorithm
         return false;
     }
 
-    public static int firstNegative (ArrayList<Integer> input)
+    public static boolean sameValueQ (ArrayList<Integer> input, ArrayList<Integer> input1)
     {
-        for (int i = 0; i < input.size(); i++)
+        for (int i : input)
         {
-            if (input.get(i) < 0)
+            for (int j : input1)
             {
-                return i;
+                if (i == j)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int firstAppear (ArrayList<Integer> input, int i)
+    {
+        for (int j = 0; j < input.size(); j++)
+        {
+            if (input.get(j) == i)
+            {
+                return j;
             }
         }
         return -1;
     }
 
-    public static boolean twoEqualQ (ArrayList<Integer> input)
+    public static int firstAppear (int[] input, int i)
     {
-        for (int i = 0; i < input.size(); i++)
+        for (int j = 0; j < input.length ; j++)
         {
-            for (int j = i + 1; j < input.size(); j++)
+            if (input[j] == i)
             {
-                if (input.get(i).equals(input.get(j)))
-                {
-                   return true;
-                }
+                return j;
             }
         }
-        return false;
+        return -1;
     }
 }
--- Main.java
+++ Main.java
@@ -1,150 +1,62 @@
-//7.4 Developing Algorithms Using ArrayLists
+//7.5 Searching
 import java.util.ArrayList;
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main (String [] args)
+    {
         ArrayList<Integer> ints1 = new ArrayList<Integer>();
-        ints1.add(1); ints1.add(2); ints1.add(2); ints1.add(3); ints1.add(3);
+        ints1.add(-1); ints1.add(0); ints1.add(1);
         ArrayList<Integer> ints2 = new ArrayList<Integer>();
-        ints2.add(1); ints2.add(1); ints2.add(1); ints2.add(2); ints2.add(3);
+        ints2.add(-1); ints2.add(-2); ints2.add(-3);
         ArrayList<Integer> ints3 = new ArrayList<Integer>();
-        ints3.add(3); ints3.add(3); ints3.add(2); ints3.add(2); ints3.add(1); ints3.add(1);
-        ArrayList<Integer> ints4 = new ArrayList<Integer>();
-        ints4.add(1); ints4.add(2); ints4.add(2); ints4.add(3); ints4.add(3);
-        ArrayList<Integer> ints5 = new ArrayList<Integer>();
-        ints5.add(1); ints5.add(2); ints5.add(2); ints5.add(3); ints5.add(3);
-        ArrayList<Integer> ints6 = new ArrayList<Integer>();
-        ints6.add(1); ints6.add(2); ints6.add(3);
+        ints3.add(1); ints3.add(2); ints3.add(3);
         ArrayList<String> str1 = new ArrayList<String>();
-        str1.add("Hello"); str1.add("world"); str1.add("!");
+        str1.add("String");
         ArrayList<String> str2 = new ArrayList<String>();
-        str2.add("Coding"); str2.add("is"); str2.add("fun"); str2.add("?");
+        str2.add("Strings"); str2.add("Stringy"); str2.add("String"); str2.add("Strung");
         ArrayList<String> str3 = new ArrayList<String>();
-        str3.add("Hi"); str3.add("there"); str3.add(".");
-        ArrayList<Integer> ints7 = new ArrayList<Integer>();
-        ints7.add(2); ints7.add(3); ints7.add(4);
-        ArrayList<Integer> ints8 = new ArrayList<Integer>();
-        ints8.add(-1); ints8.add(-2); ints8.add(-3);
-        ArrayList<Double> doubles1 = new ArrayList<Double>();
-        doubles1.add(1.5); doubles1.add(3.5); doubles1.add(5.5);
-        ArrayList<Double> doubles2 = new ArrayList<Double>();
-        doubles2.add(1.0); doubles2.add(2.0); doubles2.add(3.0); doubles2.add(4.0);
-        ArrayList<Double> doubles3 = new ArrayList<Double>();
-        doubles3.add(-1.5); doubles3.add(0.0); doubles3.add(1.5);
-        ArrayList<Integer> ints9 = new ArrayList<Integer>();
-        ints9.add(1); ints9.add(0); ints9.add(-1);
-        ArrayList<Integer> ints10 = new ArrayList<Integer>();
-        ints10.add(1); ints10.add(2); ints10.add(3); ints10.add(2);
+        str3.add("Strings"); str3.add("are"); str3.add("lame"); str3.add(".");
+        ArrayList<Integer> ints4 = new ArrayList<Integer>();
+        ints4.add(1); ints4.add(2);
+        ArrayList<Integer> ints5 = new ArrayList<Integer>();
+        ints5.add(2); ints5.add(3);
+        int[] int1 = {1, 2, 3};
+        int[] int2 = {-1, 0, 1};
+        int[] int3 = {1, 2};
 
         System.out.print(ints1 + " ");
-        Algorithm.removeConsec(ints1);
-        System.out.println(ints1);
+        System.out.println(Algorithm.nLessThan0(ints1));
         System.out.print(ints2 + " ");
-        Algorithm.removeConsec(ints2);
-        System.out.println(ints2);
+        System.out.println(Algorithm.nLessThan0(ints2));
         System.out.print(ints3 + " ");
-        Algorithm.removeConsec(ints3);
-        System.out.println(ints3);
-
-        System.out.print(ints4 + " ");
-        Algorithm.remove(ints4, 2);
-        System.out.print(ints4 + " ");
-        Algorithm.remove(ints4, 3);
-        System.out.print(ints4 + " ");
-        Algorithm.remove(ints4, 1);
-        System.out.println(ints4);
-
-        System.out.print(ints5 + " ");
-        Algorithm.addInt(ints5, 4);
-        System.out.println(ints5 + " ");
-        System.out.print(ints1 + " ");
-        Algorithm.addInt(ints1, 2);
-        System.out.println(ints1 + " ");
-        System.out.print(ints2 + " ");
-        Algorithm.addInt(ints2, 3);
-        System.out.println(ints2 + " ");
-
-        System.out.print(ints6 + " " + ints3 + " ");
-        System.out.println(Algorithm.reverseQ(ints6, ints3));
-        System.out.print(ints1 + " " + ints3 + " ");
-        System.out.println(Algorithm.reverseQ(ints1, ints3));
-        System.out.print(ints2 + " " + ints3 + " ");
-        System.out.println(Algorithm.reverseQ(ints2, ints3));
-
-        System.out.print(ints3 + " ");
-        System.out.println(Algorithm.maximum(ints3));
-        System.out.print(ints2 + " ");
-        System.out.println(Algorithm.maximum(ints2));
-        System.out.print(ints5 + " ");
-        System.out.println(Algorithm.maximum(ints5));
-
-        System.out.print(ints3 + " ");
-        System.out.println(Algorithm.minimum(ints3));
-        System.out.print(ints2 + " ");
-        System.out.println(Algorithm.minimum(ints2));
-        System.out.print(ints5 + " ");
-        System.out.println(Algorithm.minimum(ints5));
+        System.out.println(Algorithm.nLessThan0(ints3));
 
         System.out.print(str1 + " ");
-        System.out.println(Algorithm.shortest(str1));
+        System.out.println(Algorithm.inQ(str1, "String"));
         System.out.print(str2 + " ");
-        System.out.println(Algorithm.shortest(str2));
+        System.out.println(Algorithm.inQ(str2, "String"));
         System.out.print(str3 + " ");
-        System.out.println(Algorithm.shortest(str3));
+        System.out.println(Algorithm.inQ(str3, "String"));
 
-        System.out.print(str1 + " ");
-        System.out.println(Algorithm.longest(str1));
-        System.out.print(str2 + " ");
-        System.out.println(Algorithm.longest(str2));
-        System.out.print(str3 + " ");
-        System.out.println(Algorithm.longest(str3));
-
-        System.out.print(ints7 + " ");
-        System.out.print(ints8 + " ");
-        System.out.println(Algorithm.largestAbsDiff(ints7, ints8));
-        System.out.print(ints1 + " ");
-        System.out.print(ints2 + " ");
-        System.out.println(Algorithm.largestAbsDiff(ints1, ints2));
-        System.out.print(ints3 + " ");
-        System.out.print(ints8 + " ");
-        System.out.println(Algorithm.largestAbsDiff(ints3, ints8));
-
-        System.out.print(doubles1 + " ");
-        System.out.println(Algorithm.average(doubles1));
-        System.out.print(doubles2 + " ");
-        System.out.println(Algorithm.average(doubles2));
-        System.out.print(doubles3 + " ");
-        System.out.println(Algorithm.average(doubles3));
-
-        System.out.print(ints6 + " ");
-        Algorithm.reverse(ints6);
-        System.out.println(ints6);
-        System.out.print(ints1 + " ");
-        Algorithm.reverse(ints1);
-        System.out.println(ints1);
-        System.out.print(ints8 + " ");
-        Algorithm.reverse(ints8);
-        System.out.println(ints8);
-
-        ints2.add(4);
-        System.out.print(ints2 + " ");
-        System.out.println(Algorithm.consecQ(ints2));
+        System.out.print(ints4 + " ");
         System.out.print(ints5 + " ");
-        System.out.println(Algorithm.consecQ(ints5));
-        System.out.print(ints7 + " ");
-        System.out.println(Algorithm.consecQ(ints7));
-
-        System.out.print(ints9 + " ");
-        System.out.println(Algorithm.firstNegative(ints9));
-        System.out.print(ints8 + " ");
-        System.out.println(Algorithm.firstNegative(ints8));
+        System.out.println(Algorithm.sameValueQ(ints4, ints5));
         System.out.print(ints1 + " ");
-        System.out.println(Algorithm.firstNegative(ints1));
-
-        System.out.print(ints10 + " ");
-        System.out.println(Algorithm.twoEqualQ(ints10));
+        System.out.print(ints2 + " ");
+        System.out.println(Algorithm.sameValueQ(ints1, ints2));
         System.out.print(ints1 + " ");
-        System.out.println(Algorithm.twoEqualQ(ints1));
         System.out.print(ints3 + " ");
-        System.out.println(Algorithm.twoEqualQ(ints3));
+        System.out.println(Algorithm.sameValueQ(ints1, ints3));
+
+        System.out.print(ints3 + " ");
+        System.out.println(Algorithm.firstAppear(ints3, 2));
+        System.out.print(ints1 + " ");
+        System.out.println(Algorithm.firstAppear(ints1, -1));
+        System.out.print(ints4 + " ");
+        System.out.println(Algorithm.firstAppear(ints4, 3));
+
+        System.out.println(Algorithm.firstAppear(int1, 2));
+        System.out.println(Algorithm.firstAppear(int2, -1));
+        System.out.println(Algorithm.firstAppear(int3, 3));
     }
 }
