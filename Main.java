4149d2000cc485f8c86feab19c9d158b195f97ea
Julie Hum <julieehum@gmail.com>
2024-02-28 22:23:38 -0500

    7.6 Sorting

--- Main.java
+++ Main.java
@@ -1,8 +1,74 @@
+//7.6 Sorting
+import java.util.ArrayList;
 public class Main
 {
     public static void main (String [] args)
     {
-        int [] ints = {3, 2, 1};
-        Sort.selectionSort(ints);
+        String [] ssstrs1 = {"zebra", "alpha"};
+        String [] ssstrs2 = {"zebra", "alpha", "delta", "beta", "phi", "gamma", "zeta", "lambda"};
+        String [] ssstrs3 = {"bake", "cake", "make", "rake", "lake", "take", "ache", "fake", "wake", "opaque", "remake", "jake"};
+        String [] isstrs1 = {"zebra", "alpha"};
+        String [] isstrs2 = {"zebra", "alpha", "delta", "beta", "phi", "gamma", "zeta", "lambda"};
+        String [] isstrs3 = {"bake", "cake", "make", "rake", "lake", "take", "ache", "fake", "wake", "opaque", "remake", "jake"};
+        ArrayList<String> ssstrings1 = new ArrayList<String>();
+        for (String str : ssstrs1)
+        {
+            ssstrings1.add(str);
+        }
+        ArrayList<String> ssstrings2 = new ArrayList<String>();
+        for (String str : ssstrs2)
+        {
+            ssstrings2.add(str);
+        }
+        ArrayList<String> ssstrings3 = new ArrayList<String>();
+        for (String str : ssstrs3)
+        {
+            ssstrings3.add(str);
+        }
+        ArrayList<String> isstrings1 = new ArrayList<String>();
+        for (String str : isstrs1)
+        {
+            isstrings1.add(str);
+        }
+        ArrayList<String> isstrings2 = new ArrayList<String>();
+        for (String str : isstrs2)
+        {
+            isstrings2.add(str);
+        }
+        ArrayList<String> isstrings3 = new ArrayList<String>();
+        for (String str : isstrs3)
+        {
+            isstrings3.add(str);
+        }
+        ArrayList<Integer> ssints1 = new ArrayList<Integer>();
+        ssints1.add(1); ssints1.add(0); ssints1.add(-1);
+        ArrayList<Integer> ssints2 = new ArrayList<Integer>();
+        ssints2.add(3); ssints2.add(2); ssints2.add(1);
+        ArrayList<Integer> ssints3 = new ArrayList<Integer>();
+        ssints3.add(5); ssints3.add(1); ssints3.add(3); ssints3.add(9); ssints3.add(7);
+        ArrayList<Integer> isints1 = new ArrayList<Integer>();
+        isints1.add(1); isints1.add(0); isints1.add(-1);
+        ArrayList<Integer> isints2 = new ArrayList<Integer>();
+        isints2.add(3); isints2.add(2); isints2.add(1);
+        ArrayList<Integer> isints3 = new ArrayList<Integer>();
+        isints3.add(5); isints3.add(1); isints3.add(3); isints3.add(9); isints3.add(7);
+        Sort.selectionSort(ssstrs1);
+        Sort.selectionSort(ssstrs2);
+        Sort.selectionSort(ssstrs3);
+        Sort.insertionSort(isstrs1);
+        Sort.insertionSort(isstrs2);
+        Sort.insertionSort(isstrs3);
+        Sort.strSelectionSort(ssstrings1);
+        Sort.strSelectionSort(ssstrings2);
+        Sort.strSelectionSort(ssstrings3);
+        Sort.strInsertionSort(isstrings1);
+        Sort.strInsertionSort(isstrings2);
+        Sort.strInsertionSort(isstrings3);
+        Sort.intSelectionSort(ssints1);
+        Sort.intSelectionSort(ssints2);
+        Sort.intSelectionSort(ssints3);
+        Sort.intInsertionSort(isints1);
+        Sort.intInsertionSort(isints2);
+        Sort.intInsertionSort(isints3);
     }
 }
--- Sort.java
+++ Sort.java
@@ -1,6 +1,8 @@
 /** This class is a demonstration of selection sort and insertion sort
  * for AP Computer Science A.
  * @author Mr Holmer */
+//7.6 Sorting
+import java.util.ArrayList;
 public class Sort
 {
     /** Prints an array.
@@ -70,6 +72,38 @@ public class Sort
         System.out.println("Minimum Updates: " + minUpdate);
         return index;
     }
+    public static int strFindMinIndex(int index, ArrayList<String> arr)
+    {
+        int minUpdate = 0;
+        String min = arr.get(index);
+        for(int i = index; i < arr.size(); i++)
+        {
+            if(arr.get(i).compareTo(min)<0)
+            {
+                min = arr.get(i);
+                index = i;
+                minUpdate++;
+            }
+        }
+        System.out.println("Minimum Updates: " + minUpdate);
+        return index;
+    }
+    public static int intFindMinIndex(int index, ArrayList<Integer> arr)
+    {
+        int minUpdate = 0;
+        int min = arr.get(index);
+        for(int i = index; i < arr.size(); i++)
+        {
+            if(arr.get(i).compareTo(min)<0)
+            {
+                min = arr.get(i);
+                index = i;
+                minUpdate++;
+            }
+        }
+        System.out.println("Minimum Updates: " + minUpdate);
+        return index;
+    }
     /**Swaps the elements of two indices of an array
      * @param arr the array in which to swap
      * @param i the index of the first element
@@ -86,10 +120,24 @@ public class Sort
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    public static void strSwap(ArrayList<String> arr, int i, int j)
+    {
+        String temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
+    public static void intSwap(ArrayList<Integer> arr, int i, int j)
+    {
+        int temp = arr.get(i);
+        arr.set(i, arr.get(j));
+        arr.set(j, temp);
+    }
     /** Sorts an int array in ascending order with selection sort
      * @param arr the int array to be sorted */
     public static void selectionSort(int[] arr)
     {
+        System.out.println("Selection Sort");
+        print(arr);
         int swaps = 0;
         for(int i = 0; i < arr.length; i++)
         {
@@ -105,6 +153,8 @@ public class Sort
     }
     public static void selectionSort(String[] arr)
     {
+        System.out.println("Selection Sort");
+        print(arr);
         int swaps = 0;
         for(int i = 0; i < arr.length; i++)
         {
@@ -118,8 +168,44 @@ public class Sort
         }
         System.out.println("Swaps: " + swaps);
     }
+    public static void strSelectionSort(ArrayList<String> arr)
+    {
+        System.out.println("Selection Sort");
+        System.out.println(arr);
+        int swaps = 0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            int minIndex = strFindMinIndex(i, arr);
+            if(minIndex != i)
+            {
+                strSwap(arr, i, minIndex);
+                swaps++;
+            }
+            System.out.println(arr);
+        }
+        System.out.println("Swaps: " + swaps);
+    }
+    public static void intSelectionSort(ArrayList<Integer> arr)
+    {
+        System.out.println("Selection Sort");
+        System.out.println(arr);
+        int swaps = 0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            int minIndex = intFindMinIndex(i, arr);
+            if(minIndex != i)
+            {
+                intSwap(arr, i, minIndex);
+                swaps++;
+            }
+            System.out.println(arr);
+        }
+        System.out.println("Swaps: " + swaps);
+    }
     public static void insertionSort(int[] arr)
     {
+        System.out.println("Insertion Sort");
+        print(arr);
         int swaps = 0;
         for(int i = 1; i < arr.length; i++)
         {
@@ -132,6 +218,63 @@ public class Sort
                 index--;
             }
             print(arr);
+        }
+        System.out.println("Swaps: " + swaps);
+    }
+    public static void insertionSort(String[] arr)
+    {
+        System.out.println("Insertion Sort");
+        print(arr);
+        int swaps = 0;
+        for(int i = 1; i < arr.length; i++)
+        {
+            String temp = arr[i];
+            int index = i;
+            while(index > 0 && temp.compareTo(arr[index - 1])<0)
+            {
+                swap(arr, index, index - 1);
+                swaps++;
+                index--;
+            }
+            print(arr);
+        }
+        System.out.println("Swaps: " + swaps);
+    }
+    public static void strInsertionSort(ArrayList<String> arr)
+    {
+        System.out.println("Insertion Sort");
+        System.out.println(arr);
+        int swaps = 0;
+        for(int i = 1; i < arr.size(); i++)
+        {
+            String temp = arr.get(i);
+            int index = i;
+            while(index > 0 && temp.compareTo(arr.get(index - 1))<0)
+            {
+                strSwap(arr, index, index - 1);
+                swaps++;
+                index--;
+            }
+            System.out.println(arr);
+        }
+        System.out.println("Swaps: " + swaps);
+    }
+    public static void intInsertionSort(ArrayList<Integer> arr)
+    {
+        System.out.println("Insertion Sort");
+        System.out.println(arr);
+        int swaps = 0;
+        for(int i = 1; i < arr.size(); i++)
+        {
+            int temp = arr.get(i);
+            int index = i;
+            while(index > 0 && temp < arr.get(index - 1))
+            {
+                intSwap(arr, index, index - 1);
+                swaps++;
+                index--;
+            }
+            System.out.println(arr);
         }
         System.out.println("Swaps: " + swaps);
     }
