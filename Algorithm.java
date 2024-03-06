4bf9ad355a12d094daee28091c67c7cd3fcca70b
Julie Hum <julieehum@gmail.com>
2024-03-06 02:13:15 -0500

    8.2 Traversing 2D Arrays

--- Algorithm.java
+++ Algorithm.java
@@ -1,3 +1,4 @@
+//8.2 Traversing 2D Arrays
 import java.util.ArrayList;
 
 public class Algorithm
@@ -33,20 +34,249 @@ public class Algorithm
         return false;
     }
 
-    public static String firstOccurrence(int[][] input, int i)
+    public static ArrayList<Integer> firstOccurrence(int[][] input, int i)
     {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(-1); result.add(-1);
         for (int r = 0; r < input.length; r++)
         {
             for (int c = 0; c < input[0].length; c++)
             {
                 if (input[r][c] == i)
                 {
-                   return "[" + r + ", " + c + "]";
+                    result.set(0, r);
+                    result.set(1, c);
+                    return result;
                 }
             }
         }
-        return "[-1, -1]";
+        return result;
     }
 
-    public static
+    public static ArrayList<Integer> diagonal (int[][] input)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        int b = input.length;
+        if (input.length > input[0].length)
+        {
+            b = input[0].length;
+        }
+        for (int i = 0; i < b; i ++)
+        {
+            result.add(input[i][i]);
+        }
+        return result;
+    }
+
+    public static String column (String[][] input, int c)
+    {
+        String result = "";
+        for (int r = 0; r < input.length; r++)
+        {
+            result += input[r][c] + " ";
+        }
+        return result;
+    }
+
+    public static int maximum (int[][] input)
+    {
+        int max = input[0][0];
+        for (int[] r : input)
+        {
+            for (int c = 0; c < r.length; c++)
+            {
+                if (r[c] > max)
+                {
+                    max = r[c];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int minimum (int[][] input)
+    {
+        int min = input[0][0];
+        for (int[] r : input)
+        {
+            for (int c = 0; c < r.length; c++)
+            {
+                if (r[c] < min)
+                {
+                    min = r[c];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static String shortest (String[][] input)
+    {
+        String min = input[0][0];
+        for (String[] r : input)
+        {
+            for (int c = 0; c < r.length; c++)
+            {
+                if (r[c].length() < min.length())
+                {
+                    min = r[c];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static String longest (String[][] input)
+    {
+        String max = input[0][0];
+        for (String[] r : input)
+        {
+            for (int c = 0; c < r.length; c++)
+            {
+                if (r[c].length() > max.length())
+                {
+                    max = r[c];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int largestAbsDiff (int[][] input, int[][] input1)
+    {
+        int lAD = Math.abs(input[0][0] - input1[0][0]);
+        for (int r = 0; r < input.length; r++)
+        {
+            for (int c = 0; c < input[0].length; c++)
+            {
+                if (Math.abs(input[r][c]-input1[r][c]) > lAD)
+                {
+                    lAD = Math.abs((input[r][c]-input1[r][c]));
+                }
+            }
+        }
+        return lAD;
+    }
+
+    public static double mean (double[][] input)
+    {
+        double sum = 0;
+        for (double[] r : input)
+        {
+            for (int c = 0; c < input[0].length; c++)
+            {
+                sum += r[c];
+            }
+        }
+        return sum/(input.length*input[0].length);
+    }
+
+    public static boolean twoEquivQ (int[][] input)
+    {
+        int i = 0;
+        for (int r = 0; r < input.length; r++)
+        {
+            for (int c = 0; c < input[0].length; c++)
+            {
+                i = input[r][c];
+                if (c != input[0].length-1)
+                {
+                    for (int c1 = c + 1; c < input[0].length; c++)
+                    {
+                        if (input[r][c1] == i)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                if (r != input.length-1)
+                {
+                    for (int r1 = r + 1; r < input.length; r++)
+                    {
+                        for (int c2 = 0; c < input[r].length; c++)
+                        {
+                            if (input[r1][c2] == i)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int c2 = 0; c < input[r].length; c++)
+                    {
+                        if (input[r][c2] == i)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+//    public static int twoEquivQ1 (int[][] input)
+//    {
+//        int i = 0;
+//        for (int r = 0; r < input.length; r++)
+//        {
+//            System.out.println("r" + r);
+//            for (int c = 0; c < input[0].length; c++)
+//            {
+//                System.out.println("c" + c);
+//                i = input[r][c];
+//                System.out.println(i);
+//                if (c != input[0].length-1)
+//                {
+//                    for (int c1 = c + 1; c < input[0].length; c++)
+//                    {
+//                        if (input[r][c1] == i)
+//                        {
+//                            return 1;
+//                        }
+//                    }
+//                }
+//                if (r != input.length-1)
+//                {
+//                    for (int r1 = r + 1; r < input.length; r++)
+//                    {
+//                        for (int c2 = 0; c < input[0].length; c++)
+//                        {
+//                            if (input[r1][c2] == i)
+//                            {
+//                                return 2;
+//                            }
+//                        }
+//                    }
+//                } else
+//                {
+//                    for (int c2 = 0; c < input[r].length; c++)
+//                    {
+//                        if (input[r][c2] == i)
+//                        {
+//                            return 3;
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return 4;
+//    }
+
+    public static String lastAlpha (String[][] input)
+    {
+        String last = input[0][0];
+        for (int r = 0; r < input.length; r++)
+        {
+            for (int c = 0; c < input[0].length; c++)
+            {
+                if (input[r][c].compareTo(last) > 0)
+                {
+                    last = input[r][c];
+                }
+            }
+        }
+        return last;
+    }
 }
--- Main.java
+++ Main.java
@@ -5,11 +5,20 @@ public class Main
     public static void main (String [] args)
     {
         String[][] strs1 = {{"Hello", "world", "!"}, {"It's", "me", "Genevieve"}};
-        String[][] strs2 = {{"Hey", "what's", "up", "?"}, {"Just", "saying", "hi"}};
+        String[][] strs2 = {{"Hey", "what's", "up"}, {"Just", "saying", "hi"}};
         String[][] strs3 = {{"I", "like", "your", "shirt", "!"}, {"Really", "?", "Thanks", "!"}};
-        int[][]ints1 = {{1, 2}, {3, 4}};
-        int[][]ints2 = {{1, 2, 3, 4}, {3, 4, 5, 6}};
-        int[][]ints3 = {{-1, 0, 2}, {3, 4, 5, 6}};
+        String[][] strs4 = {{"Hello,", "world", "!"}, {"It's", "me", "Margaret"}};
+        String[][] strs5 = {{"bravo", "charlie"}, {"zebra", "alpha"}};
+        int[][] ints1 = {{1, 2}, {3, 4}};
+        int[][] ints2 = {{1, 2, 3, 4}, {3, 4, 5, 6}};
+        int[][] ints3 = {{-2, -1, 0, 2}, {3, 4, 5, 6}};
+        int[][] ints4 = {{3, 2, 1}, {4, 5, 6}};
+        int[][] ints5 = {{2, 3, 4}, {-1, -2, -3}};
+        int[][] ints6 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] ints7 = {{1, 2},{3, 2}};
+        double[][] doubles1 = {{1.6, 3.2, 5.4}, {6.3, 4.1, 2.5}};
+        double[][] doubles2 = {{1.6, 3.2, 5.4}, {6.3, 4.1, 2.5}, {4.0, 6.0, 9.0}};
+        double[][] doubles3 = {{2.9, 3.1, 2.0}, {3.5, 1.1, -0.8}, {-4.2, 3.1, 2.2}};
         System.out.println(Algorithm.numOf(strs1, "e"));
         System.out.println(Algorithm.numOf(strs2, "y"));
         System.out.println(Algorithm.numOf(strs3, "!"));
@@ -19,5 +28,35 @@ public class Main
         System.out.println(Algorithm.firstOccurrence(ints1, 3));
         System.out.println(Algorithm.firstOccurrence(ints2, 4));
         System.out.println(Algorithm.firstOccurrence(ints3, 1));
+        System.out.println(Algorithm.diagonal(ints1));
+        System.out.println(Algorithm.diagonal(ints2));
+        System.out.println(Algorithm.diagonal(ints3));
+        System.out.println(Algorithm.column(strs4, 1));
+        System.out.println(Algorithm.column(strs1, 2));
+        System.out.println(Algorithm.column(strs2, 1));
+        System.out.println(Algorithm.maximum(ints4));
+        System.out.println(Algorithm.maximum(ints2));
+        System.out.println(Algorithm.maximum(ints3));
+        System.out.println(Algorithm.minimum(ints4));
+        System.out.println(Algorithm.minimum(ints2));
+        System.out.println(Algorithm.minimum(ints3));
+        System.out.println(Algorithm.shortest(strs4));
+        System.out.println(Algorithm.shortest(strs1));
+        System.out.println(Algorithm.shortest(strs2));
+        System.out.println(Algorithm.longest(strs4));
+        System.out.println(Algorithm.longest(strs1));
+        System.out.println(Algorithm.longest(strs2));
+        System.out.println(Algorithm.largestAbsDiff(ints5, ints6));
+        System.out.println(Algorithm.largestAbsDiff(ints2, ints3));
+        System.out.println(Algorithm.largestAbsDiff(ints4, ints6));
+        System.out.println(Algorithm.mean(doubles1));
+        System.out.println(Algorithm.mean(doubles2));
+        System.out.println(Algorithm.mean(doubles3));
+        System.out.println(Algorithm.twoEquivQ(ints7));
+        System.out.println(Algorithm.twoEquivQ(ints1));
+        System.out.println(Algorithm.twoEquivQ(ints2));
+        System.out.println(Algorithm.lastAlpha(strs5));
+        System.out.println(Algorithm.lastAlpha(strs1));
+        System.out.println(Algorithm.lastAlpha(strs2));
     }
 }
